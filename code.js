78514d945dea3ddfec586ef3c4afb6f79d74fef1
gospelvideoquotes <dallins@juno.com>
2016-04-27 16:16:04 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,8 @@
 	var JesusImageNumber = ["001","002","003","004","005","006","007","008","009","010","011","012","013","014","015","016","017","018","019","020","021","022","023","024","025","026","027","028","029","030","031","032","033","034","035","036","037","038","039","040","041","042","043","044","045","046","047","048","049","050","051","052","053","054","055","056","057","058","059","060","061","062","063","064","065","066","067","068","069","070","071","072","073","074","075","076","077","078","079","080","081","082","083","084","085","086","087","088","089","090","091","092","093","094","095","096","097","098","099","100","101","102","103","104","105","106","107","108","109","110","111"];
 
 	var spriteSource = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAArwAAAGJAQMAAABSK3KDAAAAA1BMVEX///+nxBvIAAAAAXRSTlMAQObYZgAAADlJREFUeNrtwTEBAAAAwqD1T20ND6AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAOwOIoQABlnB/ewAAAABJRU5ErkJggg==";
-
-	var step = Math.floor(Math.random()*(110+1)); // This includes step 0 and the left number is the highest number than can be drawn. -->
+var step = 1;
+//	var step = Math.floor(Math.random()*(110+1)); This includes step 0 and the left number is the highest number than can be drawn.
 
 	var set;
 
